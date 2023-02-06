30e101c2599c0f18f9846740cb7fa8ec937a6272
Oliver03710 <jhee2619@gmail.com>
2023-02-06 20:37:09 +0900

    Feat: Endpoints 추가

---
+++ MediaTrending/Utilities/Networking/EndPoints/EndPoints.swift
@@ -0,0 +1,12 @@
+//
+//  EndPoints.swift
+//  MediaTrending
+//
+//  Created by Junhee Yoon on 2023/02/06.
+//
+
+import Foundation
+
+enum EndPoints {
+    static let TMDBUrl = "https://api.themoviedb.org/3/"
+}
