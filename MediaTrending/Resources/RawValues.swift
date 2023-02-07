9bd8b6dce53bcc13b5c8aeeba69ed9aa0a5d868c
Oliver03710 <jhee2619@gmail.com>
2023-02-07 16:25:44 +0900

    Feat: Raw값 관리할 Enum 생성

---
+++ MediaTrending/Resources/RawValues.swift
@@ -0,0 +1,20 @@
+//
+//  RawValues.swift
+//  MediaTrending
+//
+//  Created by Junhee Yoon on 2023/02/06.
+//
+
+import Foundation
+
+enum Raws {
+    
+    // MARK: - Networking
+    enum Networking {
+        
+        enum Period {
+            static let day = "/day?"
+            static let week = "/week?"
+        }
+    }
+}
