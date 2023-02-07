5b863da8d6d1156cd89e41a01403d71b45f312f6
Oliver03710 <jhee2619@gmail.com>
2023-02-07 21:11:41 +0900

    Feat: 출연진 정보 DTO 추가

---
+++ MediaTrending/Utilities/Networking/DTOs/Casts.swift
@@ -0,0 +1,33 @@
+//
+//  Casts.swift
+//  MediaTrending
+//
+//  Created by Junhee Yoon on 2023/02/07.
+//
+
+import Foundation
+
+struct CastResponse: Codable {
+    let cast: [Cast]
+}
+
+struct Cast: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let profilePath: String?
+    let character: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case profilePath = "profile_path"
+        case character
+    }
+    
+    static func getDummy() -> [Self] {
+        return [Cast(id: 1, name: "Dummy1", profilePath: "/hoP3vT6OBMNCUFTqlTDcd2X3y77.jpg", character: "Joel Miller"),
+                Cast(id: 2, name: "Dummy2", profilePath: "/hoP3vT6OBMNCUFTqlTDcd2X3y77.jpg", character: "Joel Miller"),
+                Cast(id: 3, name: "Dummy3", profilePath: "/hoP3vT6OBMNCUFTqlTDcd2X3y77.jpg", character: "Joel Miller"),
+                Cast(id: 4, name: "Dummy4", profilePath: "/hoP3vT6OBMNCUFTqlTDcd2X3y77.jpg", character: "Joel Miller")]
+    }
+}
