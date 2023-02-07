1fe0b36a933d48696012904c9461f29e757681ec
Oliver03710 <jhee2619@gmail.com>
2023-02-07 21:15:06 +0900

    Feat: 영화 정보와 출연진 정보 가져오기

--- MediaTrending/Scenes/Main/ViewModels/MainViewModel.swift
+++ MediaTrending/Scenes/Main/ViewModels/MainViewModel.swift
@@ -14,31 +14,49 @@ final class MainViewModel: ObservableObject {
     
     var subscription = Set<AnyCancellable>()
     
-    @Published var movies = [Movies]()
-    private let url = EndPoints.TMDBUrl + URLs.tv.rawValue + Raws.Networking.Period.day + "api_key=" + APIKey.TMDB.rawValue + "&page=\(1)"
+    @Published var movies = [Movie]()
+    @Published var casts = [Int: [Cast]]()
+    
+    private let mediaUrl = EndPoints.mainUrl + "trending/" + URLs.tv.rawValue + Raws.Networking.Period.day + "api_key=" + APIKey.TMDB.rawValue + "&page=\(1)"
     
     init() {
         fetchMovies()
     }
     
     func fetchMovies() {
-        print(url)
-        AF.request(url)
-            .publishDecodable(type: Movies.self)
+        AF.request(mediaUrl)
+            .publishDecodable(type: MovieResponse.self)
             .compactMap { $0.value }
             .map { $0.results }
             .sink(receiveCompletion: { completion in
                 print("Networking Completed‼️")
                 
-            }, receiveValue: { receivedValue in
+            }, receiveValue: { [weak self] receivedValue in
                 print("받은 값: \(receivedValue.count)")
-                dump(receivedValue)
+                receivedValue
+                    .forEach { movie in
+                        self?.movies.append(movie)
+                        self?.fetchCrews(id: movie.id)
+                    }
+                
             })
             .store(in: &subscription)
     }
     
-    
-    
-    
-    
+    func fetchCrews(id: Int) {
+        let crewUrl = EndPoints.mainUrl + URLs.tv.rawValue + "/\(id)/credits?api_key=\(APIKey.TMDB.rawValue)&language=en-US"
+        
+        AF.request(crewUrl)
+            .publishDecodable(type: CastResponse.self)
+            .compactMap { $0.value }
+            .map { $0.cast }
+            .sink { completion in
+                print("Networking Completed‼️")
+                
+            } receiveValue: { receivedValue in
+                print("받은 값: \(receivedValue)")
+                self.casts.updateValue(receivedValue, forKey: id)
+            }
+            .store(in: &subscription)
+    }
 }
