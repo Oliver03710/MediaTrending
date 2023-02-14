742d653885bc046716222b98d14cd58dcb58a376
Oliver03710 <jhee2619@gmail.com>
2023-02-14 20:35:27 +0900

    Style: Test를 위한 Dummy Data 값 변경

--- MediaTrending/Utilities/Networking/DTOs/Movie.swift
+++ MediaTrending/Utilities/Networking/DTOs/Movie.swift
@@ -47,6 +47,6 @@ struct Movie: Codable, Identifiable, MediaType {
     }
     
     static func getDummy() -> Self {
-        return Movie(adult: false, id: 1, title: "Dummy", overview: "Overview", date: "2023-01-15", posterPath: "/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg", backdropPath: "/uDgy6hyPd82kOHh6I95FLtLnj6p.jpg", genreIDS: [18, 10759], voteAverage: 8.836)
+        return Movie(adult: false, id: 1, title: "Dummy", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", date: "2023-01-15", posterPath: "/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg", backdropPath: "/uDgy6hyPd82kOHh6I95FLtLnj6p.jpg", genreIDS: [18, 10759], voteAverage: 8.836)
     }
 }
