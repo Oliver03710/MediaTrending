81b0d3729d296b304bb36de4115eda650d3d631e
Oliver03710 <jhee2619@gmail.com>
2023-02-14 20:42:05 +0900

    Feat: Detail Cell View를 이용한 기본 화면 구성

--- MediaTrending/Scenes/Detail/Views/DetailView.swift
+++ MediaTrending/Scenes/Detail/Views/DetailView.swift
@@ -9,18 +9,47 @@ import SwiftUI
 
 struct DetailView: View {
     
-    var casts: [Cast]
-    var crews: [Crew]
-    
-    @ObservedObject var viewModel = DetailViewModel()
+    @ObservedObject var viewModel: DetailViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(header: Text("Overview")) {
+                Text("\(viewModel.media.overview)")
+            }
+            .textCase(nil)
+            
+            Section(header: Text("Casts")) {
+                ForEach(viewModel.casts, id: \.self) { item in
+                    LazyVStack(alignment: .leading) {
+                        DetailCellView(imagePath: item.profilePath,
+                                       name: item.name,
+                                       position: item.character)
+                    }
+                }
+            }
+            .textCase(nil)
+            
+            Section(header: Text("Crews")) {
+                ForEach(viewModel.crews, id: \.self) { item in
+                    LazyVStack(alignment: .leading) {
+                        DetailCellView(imagePath: item.profilePath,
+                                       name: item.name,
+                                       position: item.knownForDepartment)
+                    }
+                }
+            }
+            .textCase(nil)
+        }
+        
     }
 }
 
+
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(casts: Cast.getDummy(), crews: Crew.getDummy())
+        DetailView(viewModel: DetailViewModel(3696814,
+                                              media: Movie.getDummy(),
+                                              casts: Cast.getDummy(),
+                                              crews: Crew.getDummy()))
     }
 }
