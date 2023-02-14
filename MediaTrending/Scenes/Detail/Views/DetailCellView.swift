921dbc78f1bda8704366844456fa19df38d38cf8
Oliver03710 <jhee2619@gmail.com>
2023-02-14 20:09:05 +0900

    Feat: Detail View에 들어갈 공통항목의 모듈화를 위한 View 생성

---
+++ MediaTrending/Scenes/Detail/Views/DetailCellView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailCellView.swift
+//  MediaTrending
+//
+//  Created by Junhee Yoon on 2023/02/14.
+//
+
+import SwiftUI
+
+struct DetailCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailCellView()
+    }
+}
