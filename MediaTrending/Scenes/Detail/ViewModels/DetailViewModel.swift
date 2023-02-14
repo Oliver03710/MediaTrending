f426f3513d2a7c349abacb6f7dca50207f988721
Oliver03710 <jhee2619@gmail.com>
2023-02-14 18:50:19 +0900

    Feat: 상세 화면의 ViewModel 추가

---
+++ MediaTrending/Scenes/Detail/ViewModels/DetailViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewModel.swift
+//  MediaTrending
+//
+//  Created by Junhee Yoon on 2023/02/14.
+//
+
+import Foundation
+import Combine
+
+final class DetailViewModel: ObservableObject {
+    
+    var subscription = Set<AnyCancellable>()
+    
+    init() {
+        
+    }
+}
