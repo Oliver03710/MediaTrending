f038b3a7cc0742b4693ff72ca733e3cf3aa0e860
Oliver03710 <jhee2619@gmail.com>
2023-02-22 02:39:11 +0900

    Feat: Trailer 영상을 보여주기 위한 WebView 추가

---
+++ MediaTrending/Utilities/Networking/Managers/WebView.swift
@@ -0,0 +1,27 @@
+//
+//  WebView.swift
+//  MediaTrending
+//
+//  Created by Junhee Yoon on 2023/02/21.
+//
+
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    let request: URLRequest
+    private var webView: WKWebView?
+    
+    init(request: URLRequest) {
+        self.webView = WKWebView()
+        self.request = request
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return webView!
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(request)
+    }
+}
